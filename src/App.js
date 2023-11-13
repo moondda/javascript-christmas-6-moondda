70deeff6d51560d310837ca4984bec60b0c774ed
Moon Dahyun <dahyun723@ajou.ac.kr>
2023-11-14 07:41:22 +0900

    refactor: run 에서 printFormattedInputs printBenefits 분리

--- src/App.js
+++ src/App.js
@@ -7,19 +7,39 @@ class App {
     OutputView.printStart();
     const day = await InputView.readDate();
     const total_menu = await InputView.readMenu();
+    const price_before_benefit = await this.printFormattedInputs(total_menu);
+    await OutputView.printFreeGift(price_before_benefit);
+    const total_benefit = await this.printBenefits(
+      day,
+      total_menu,
+      price_before_benefit
+    );
+    await OutputView.printAfterBenefitPrice(
+      price_before_benefit,
+      total_benefit
+    );
+
+    await OutputView.printBadge(total_benefit);
+  }
+
+  async printFormattedInputs(total_menu) {
     await OutputView.printPreview();
     await OutputView.printMenu(total_menu);
     await OutputView.printBeforeBenefitPriceHeader();
     const price_before_benefit = await OutputView.printBeforeBenefitPrice(
       total_menu
     );
-    await OutputView.printFreeGift(price_before_benefit);
+    return price_before_benefit;
+  }
+
+  async printBenefits(day, total_menu, price_before_benefit) {
     await OutputView.printBenefitHeader();
     const dday_sale_price = await OutputView.printDdaySale(day);
     let week_sale_price;
     if (isWeekEnd)
       week_sale_price = await OutputView.printWeekdaySale(day, total_menu);
     else week_sale_price = await OutputView.printWeekendSale(day, total_menu);
+
     const star_day_sale_price = await OutputView.printStarDaySale(day);
     await OutputView.printFreeGiftEvent(price_before_benefit);
 
@@ -29,12 +49,7 @@ class App {
       star_day_sale_price
     );
 
-    await OutputView.printAfterBenefitPrice(
-      price_before_benefit,
-      total_benefit
-    );
-
-    await OutputView.printBadge(total_benefit);
+    return total_benefit;
   }
 }
 
